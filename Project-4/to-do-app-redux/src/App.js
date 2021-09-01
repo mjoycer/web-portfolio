0852b74b5e9dea90d99dc60d33195202d6a56838
rmjm <recto_mary@yahoo.com>
2021-09-01 13:12:48 +0800

    feat: add project 4: todoapp

---
+++ Project-4/to-do-app-redux/src/App.js
@@ -0,0 +1,34 @@
+import { useDispatch, useSelector } from 'react-redux';
+import './App.css';
+import TaskForm from './components/TaskForm';
+
+const App = () => {
+  const tasks = useSelector(state => state.tasks);
+  const dispatch = useDispatch();
+  console.log(tasks);
+
+
+  let doneTasks = tasks.filter((task) => task.status === "Done");
+  let pendingTasks = tasks.filter((task) => task.status === "Pending");
+
+  return (
+    <div className="App">
+      <h1>My To-do App</h1>
+      <TaskForm />
+      <div className="taskContainer">
+      <h2>Pending</h2>
+      {pendingTasks.length === 0 ?
+        <p>No Pending Tasks</p>
+        : pendingTasks.map(task => <li>{task.name}<img src="https://image.flaticon.com/icons/png/512/148/148767.png" alt="Done" onClick={() => dispatch({type: 'PENDING_TO_DONE', payload: task.id })}/><img src="https://image.flaticon.com/icons/png/512/148/148766.png" alt="Delete" onClick={() => dispatch({type: 'DELETE_TASK', payload: task.id})}/></li>)}
+      </div>
+      <div className="taskContainer">
+      <h2>Done</h2>
+      {doneTasks.length === 0 ?
+        <p>No Done Tasks</p> 
+        : doneTasks.map(task => <li>{task.name}<img src="https://image.flaticon.com/icons/png/512/148/148766.png" alt="Delete" onClick={() => dispatch({type: 'DELETE_TASK', payload: task.id})}/></li>)}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ Project-4/to-do-app-redux/src/components/TaskForm.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+const TaskForm = () => {
+    const tasks = useSelector(state => state.tasks);
+    const dispatch = useDispatch();
+    const [newTask, setNewTask] = useState([]);
+
+    const onAddTask = () => {
+        let included = false;
+        let isEmpty = true;
+
+        if(newTask.length > 0){
+            isEmpty = false;
+        }
+
+        console.log(isEmpty);
+
+        (isEmpty === false) ? tasks.map(task => {
+            if (task.name.toLowerCase() === newTask.toLowerCase()) { included = true; } return task;
+        }) : included = false;
+
+        if ((!included) && (!isEmpty) ){
+            dispatch({ type: 'ADD_TASK', payload: newTask });
+            setNewTask('');
+        } else if (isEmpty) {
+            alert('This field is required.')
+        } else {
+            alert('Duplicate Item');
+        }
+    }
+
+    const onSubmitHandle = (e) => {
+        e.preventDefault();
+        e.target.reset();
+    }
+
+    const onChangeHandle = (e) => {
+        setNewTask(e.target.value);
+    }
+
+    console.log(newTask);
+    return (
+        <div className="newTaskContainer">
+            <form onSubmit={onSubmitHandle}>
+                <input type="text" value={newTask} onChange={(e) => onChangeHandle(e)} />
+                <button onClick={onAddTask}>Add Task</button>
+            </form>
+
+        </div>
+    )
+}
+
+export default TaskForm;
