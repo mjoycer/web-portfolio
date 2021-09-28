0198ca586df464b04136182c40b4e0e5c4a82820
rmjm <recto_mary@yahoo.com>
2021-09-29 04:51:42 +0800

    feat: add portfolio

--- Project-6/roommate-express-app/models/rooms.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const Schema = mongoose.Schema;
-const RoomSchema = new Schema({
-    roomCode: String,
-    password: String
-});
-
-module.exports = mongoose.model('rooms', RoomSchema);
